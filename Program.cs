848897af72481446b2c4bb58fce97e09b273e0ac
Rodrigo Ramirez <rodrigopinheiroramirez@gmail.com>
2026-02-25 21:02:42 -0300

    add arquivos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Informe o primeiro número: ");
+int primeiroNumero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o segundo número: ");
+int segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+double adicao = primeiroNumero + segundoNumero;
+double subtracao = primeiroNumero - segundoNumero;
+double multiplicacao = primeiroNumero * segundoNumero;
+double divisao = primeiroNumero/segundoNumero;
+
+//Concatenação
+Console.WriteLine("A soma dos dois número é: " + adicao);
+
+//Interpolação
+Console.WriteLine($"A subtração entre os dois números é: {subtracao}");
+
+//Placeholders
+Console.WriteLine(string.Format("A multiplicação entre os dois números é: {0}", multiplicacao));
+Console.WriteLine(string.Format("A divisão entre os dois números é: {1}", divisao));
